6549009bf7d4a91e84f05689ed033ead2a147cd4
Vishal Pandey <vishalp055@gmail.com>
2020-11-07 15:39:02 +0530

    Add Unity Project

---
+++ Assets/Scripts/MoveToMouse.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToMouse : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private Vector3 lookAtTarget;
+    private Quaternion playerRot;
+    private float rotSpeed = 5;
+    private float speed = 50;
+    private bool isMoving = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SetPosition();
+        }
+        if(isMoving)
+            Move();
+    }
+
+    private void SetPosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 1000))
+        {
+            targetPosition = hit.point;
+            lookAtTarget = new Vector3(targetPosition.x - this.transform.position.x,
+                this.transform.position.y,
+                targetPosition.z - this.transform.position.z);
+            playerRot = Quaternion.LookRotation(lookAtTarget);
+            isMoving = true;
+        }
+    }
+
+    private void Move()
+    {
+        this.transform.rotation = Quaternion.Slerp(this.transform.rotation, 
+            playerRot, 
+            rotSpeed * Time.deltaTime);
+        this.transform.position = Vector3.MoveTowards(this.transform.position,
+            targetPosition,
+            speed * Time.deltaTime);
+        if (Vector3.Distance(this.transform.position, targetPosition) <= 2)
+            isMoving = false;
+    }
+}
---
+++ Assets/Scripts/SpawnCubes.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject spawnerPrefab;
+    public GameObject cubeParent;
+    private GameObject cubeObject;
+    private int spawnNumber;
+    public Text userInput;
+    int i=0;
+    private float radius;
+
+    bool isSpawning = false;
+
+    private void Start()
+    {
+        radius = 10;
+    }
+
+    private void FixedUpdate()
+    {
+        if (isSpawning)
+        {
+            spawnNumber = int.Parse(userInput.text.ToString());
+            if (i < spawnNumber)
+            {
+                cubeObject = Instantiate(spawnerPrefab, cubeParent.transform) as GameObject;
+
+                cubeObject.transform.position = new Vector3(transform.position.x +
+                    Random.Range(-1f, 1f) * radius,
+                    2f, transform.position.z +
+                    Random.Range(-1f, 1f) * radius);
+
+                i++;
+            }
+        }
+        
+    }
+    public void Spawn()
+    {
+        isSpawning = true;  
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
